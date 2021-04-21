7ee57619b14057be695f18e7732f191dd3de43a4
Kristina <tina91kris11@gmail.com>
2021-04-21 14:16:40 +0200

    Navigation styling - done.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@
 const Footer = () => {
   return (
     <footer className='footer'>
+      <p>Background picture taken from <a href='https://pngtree.com/so/pagedecorative'>pngtree.com</a></p>
       &copy; 2021
     </footer>
   )
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <nav className='nav'>
       <ul className="nav-list">
-        <NavLink to='/' activeClassName='active' className="nav-list-item">Home</NavLink>
+        <NavLink to='/' className="nav-list-item">Home</NavLink>
         <NavLink to='/dreams' className="nav-list-item">All Dreams</NavLink>
         <NavLink to='/add-dream' className="nav-list-item">Add A Dream</NavLink>
       </ul>
