d251a83c092eb498034207dea6445099a7b3b50a
Kristina <tina91kris11@gmail.com>
2021-04-22 17:38:29 +0200

    Added social icons to Footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import github from '../img/github.png';
+import linkedin from '../img/linedin.png';
 
 const Footer = () => {
   return (
@@ -5,7 +7,15 @@ const Footer = () => {
       <div>Background picture taken from <a href='https://pngtree.com/so/pagedecorative'>pagedecorative - pngtree.com</a></div>
       <div>Home and AddADream picture taken from <a href='https://pngtree.com/so/ripped-paper'>ripped paper - pngtree.com</a></div>
       <div>AllDreams picture taken from <a href='https://pngtree.com/so/dream-catcher-clipart'>dream catcher - pngtree.com</a></div>
-      &copy; 2021
+      <b>Made by Kristina &copy; 2021</b>
+      <div className='social'>
+        <a href='https://github.com/Kristina-11'>
+          <img src={github} />
+        </a>
+        <a href='https://www.linkedin.com/in/kristina91jovanovic11/'>
+          <img src={linkedin} />
+        </a>
+      </div>
     </footer>
   )
 }
--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,8 +1,12 @@
+import pic from '../img/adddream.png';
 
 const NotFound = () => {
   return (
     <div className='not-found'>
       <h1>Not Found</h1>
+      <div className="main-home-picture">
+        <img src={pic} />
+      </div>
     </div>
   )
 }
