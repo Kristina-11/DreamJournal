2f24e62bded587d86ff84e78f6a0b9a495d74e82
Kristina <tina91kris11@gmail.com>
2021-04-22 17:52:46 +0200

    AddADream route - added state and handleSubmit function.

--- src/components/main/AddADream.tsx
+++ src/components/main/AddADream.tsx
@@ -1,29 +1,41 @@
+import { useState } from 'react';
 import add_dream from '../../img/adddreams.png';
 
 const AddADream = () => {
+  const [ title, setTitle ] = useState('');
+  const [ description, setDescription ] = useState('');
+  const [ date, setDate ] = useState('');
+  const [ type, setType ] = useState('');
+
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(title, description, date, type);
+  }
+
   return (
     <div className='main-add-dream'>
       <h1>Add A Dream</h1>
       <div className='all-dreams dream-form'>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='form-items'>
             <label htmlFor='title'>Title</label>
-            <input type="text" name="title" id="title" required/>
+            <input type="text" name="title" id="title" onChange={(e) => setTitle(e.target.value)} required/>
           </div>
 
           <div className='form-items'>
             <label htmlFor='description'>Description</label>
-            <input type="text" name="description" id="description" required/>
+            <input type="text" name="description" id="description" onChange={(e) => setDescription(e.target.value)} required/>
           </div>
 
           <div className='form-items'>
             <label htmlFor='date'>Date</label>
-            <input type='date' name="date" id="date" required/>
+            <input type='date' name="date" id="date" onChange={(e) => setDate(e.target.value)} required/>
           </div>
 
           <div className='form-items'>
             <label htmlFor='type'>Type of dream</label>
-            <select name="type" id="type" required>
+            <select name="type" id="type" onChange={(e) => setType(e.target.value)} required>
               <option value='0'>Choose</option>
               <option value='Happy'>Happy</option>
               <option value='Sad'>Sad</option>
