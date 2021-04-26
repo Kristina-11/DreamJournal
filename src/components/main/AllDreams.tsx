03088fe60f4262af157029ec677a6e7414fd3e1a
Kristina <tina91kris11@gmail.com>
2021-04-26 13:19:24 +0200

    Added axios to the package and made minor css changes.

--- src/components/main/AllDreams.tsx
+++ src/components/main/AllDreams.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import all_dreams from '../../img/alldreams.png';
 import ADream from "./ADream";
 
-const AllDreams = () => {
+const AllDreams = (props:unknown) => {
   const [data, setData] = useState([
     { id: '1', title: 'Dream 1', description: 'Lorem ipsum something easy', date: '12/12/2020', type: 'sad' },
     { id: '2', title: 'Dream 2', description: 'Lorem ipsum something easy', date: '12/12/2020', type: 'sad' },
@@ -10,6 +10,7 @@ const AllDreams = () => {
     { id: '4', title: 'Dream 4', description: 'Lorem ipsum something easy', date: '12/12/2020', type: 'sad' }
   ]);
 
+  console.log(props)
   return (
     <div className='main-all-dreams'>
       <h1>List of Dreams</h1>
