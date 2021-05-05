d8afdbf474ddaa52060134d3aa0ac0949a8f56a7
Kristina <tina91kris11@gmail.com>
2021-05-05 18:50:49 +0200

    Update a dream - done.

--- src/components/main/DreamDetails.tsx
+++ src/components/main/DreamDetails.tsx
@@ -2,9 +2,9 @@ import { connect, useDispatch } from "react-redux";
 import { useHistory, useParams } from "react-router";
 import { IDream } from "../../react-app-env";
 import adream from '../../img/adreama.png';
-import { getDreams, postRequestSuccess, requestFailed, requestSuccess } from '../../redux/dreams/dreamActionCreator';
+import { getDreams, requestFailed } from '../../redux/dreams/dreamActionCreator';
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // TODO: Move this to react-app-env.d.ts
 type dreamParam = {
@@ -16,15 +16,21 @@ const DreamDetails = ({ dreams }: any) => {
   const data = dreams.data;
   const loading = dreams.loading;
   const error = dreams.error;
+  const dream = data.filter(( d:IDream ) => d._id === id);
 
   let dispatch = useDispatch();
   let history = useHistory();
+
+  const [ update, setUpdate ] = useState(false);
+  const [ title, setTitle ] = useState(dream[0].title);
+  const [ description, setDescription ] = useState(dream[0].description);
+  const [ date, setDate ] = useState(dream[0].date);
+  const [ type, setType ] = useState(dream[0].type);
 
   useEffect(() => {
     dispatch(getDreams())
   }, [])
 
-  const dream = data.filter(( d:IDream ) => d._id === id);
 
   const dreamTypes = (number: number) => {
     switch(number) {
@@ -55,13 +61,32 @@ const DreamDetails = ({ dreams }: any) => {
     })
   }
 
+  const handleEdit = () => {
+    setUpdate(true)
+  }
+
+  const updateFields = () => {
+    console.log(title, date, description, type)
+
+
+    axios.patch(`https:/dreamsapi.herokuapp.com/dreams/${id}`, { title, description, date, type })
+      .then((res) => {
+        history.push('/dreams')
+      })
+      .catch((err) => {
+        dispatch(requestFailed(err))
+      })
+    setUpdate(false)
+  }
+
   return (
     <div className='dream-details'>
       {
         loading ? 
           <div className='loading'>Loading...</div> :
           error ? <div className='error'> { error }</div> :
-          <>
+          ( !update ? 
+          <> 
             <h1>{dream[0].title}</h1>
             <h3>{dream[0].date}</h3>
 
@@ -77,10 +102,40 @@ const DreamDetails = ({ dreams }: any) => {
             </div>
 
             <div className="dream-details-buttons additional">
-              <button>Edit</button>
+              <button onClick={() => handleEdit()}>Edit</button>
               <button onClick={() => handleDelete(dream[0]._id)}>Delete</button>
             </div>
-          </>
+          </> : 
+          <div className='all-dreams dream-form'>
+            <div className="form-items">
+              <label htmlFor="title">Title:</label>
+              <input type='text' placeholder={dream[0].title} onChange={(e) => setTitle(e.target.value)} />
+            </div>
+
+            <div className="form-items">
+              <label htmlFor="date">Date:</label>
+              <input type="date" placeholder={dream[0].date} onChange={(e) => {setDate(e.target.value)}} />
+            </div>
+
+            <div className="form-items">
+              <label htmlFor='description'>Description</label>
+              <input type="text" placeholder={dream[0].description} onChange={(e) => setDescription(e.target.value)} />
+            </div>
+
+            <div className="form-items">
+              <label htmlFor='type'>Type of dream</label>
+              <select placeholder={dream[0].type} onChange={(e) => setType(e.target.value)}>
+                <option value='happy'>Happy</option>
+                <option value='sad'>Sad</option>
+                <option value='exciting'>Exciting</option>
+                <option value='scary'>Scary</option>
+              </select>
+            </div>
+
+            <div className="dream-details-buttons additional">
+              <button onClick={updateFields}>Done</button>
+            </div>
+          </div> )
       }
       <div className="main-home-picture details-picture">
         <img src={adream} /> 
